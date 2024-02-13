67f5bb5a3c4f5beffd70115a52822ecd92447833
nrsyzwni <syazwanimsubri@gmail.com>
2024-02-13 14:23:37 +0800

    fix admin part

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -16,12 +16,12 @@
 	<!-- Google Font -->
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<!-- CSS -->
-		<link rel="stylesheet" type="text/css" href="staff\vendors\styles\core.css">
-	<link rel="stylesheet" type="text/css" href="staff\vendors\styles\icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="src\plugins\jquery-steps\jquery.steps.css">
-	<link rel="stylesheet" type="text/css" href="src\plugins\datatables\css\dataTables.bootstrap4.min.css">
-	<link rel="stylesheet" type="text/css" href="src\plugins\datatables\css\responsive.bootstrap4.min.css">
-	<link rel="stylesheet" type="text/css" href="staff\vendors\styles\style.css">
+		<link rel="stylesheet" type="text/css" href="..\staff\vendors\styles\core.css">
+	<link rel="stylesheet" type="text/css" href="..\staff\vendors\styles\icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href=".\src\plugins\jquery-steps\jquery.steps.css">
+	<link rel="stylesheet" type="text/css" href=".\src\plugins\datatables\css\dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" type="text/css" href=".\src\plugins\datatables\css\responsive.bootstrap4.min.css">
+	<link rel="stylesheet" type="text/css" href="..\staff\vendors\styles\style.css">
 
 
 
